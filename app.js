48b140dc8706e7dca8f0ab4a49331939a8ab9c47
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-07-25 03:42:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ socket.on("gameover", (killed) => {
 });
 
 function load() {
-	alert("You have been drafted to fight in the Invisible War. Use arrow keys to move and space to launch a bullet. When you hear a boing sound that is not yours, you will have 5 seconds to run until you are out of range of your target. The catch? No enemy can be seen. Use the coordinates on the left hand side to help you. The range is how far back (-Z) you can be, however, your X must be exact. Kill the 25 enemies to get to PvP. Good luck...");
+	alert("You have been drafted to fight in the Invisible War. Use arrow keys to move and space to launch a bullet. When you hear a gunshot sound that is not yours, you will have 5 seconds to run until you are out of range of your target. The catch? No enemy can be seen. Use the coordinates on the left hand side to help you. The range is how far back (-Z) you can be, however, your X must be exact. Kill the 25 enemies to get to PvP. Good luck...");
 		choose();
 	function myfunction(tree) {
 		tree.src = "explosion.png";
